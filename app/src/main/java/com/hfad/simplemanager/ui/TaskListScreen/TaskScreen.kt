557369878e3b991df61891d63fa18d416a9a9b7c
Nikita <nantonenko59@gmail.com>
2022-07-08 09:31:28 +0300

    add HorizontalPager for task lists

--- app/src/main/java/com/hfad/simplemanager/ui/TaskListScreen/TaskScreen.kt
+++ app/src/main/java/com/hfad/simplemanager/ui/TaskListScreen/TaskScreen.kt
@@ -1,24 +1,31 @@
 package com.hfad.simplemanager.ui.TaskListScreen
 
+import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 import com.hfad.simplemanager.R
 import com.hfad.simplemanager.ui.TaskScreenEvent
 import com.hfad.simplemanager.ui.components.ChangeValueMenu
@@ -27,15 +34,21 @@ import com.hfad.simplemanager.ui.components.TransparentButton
 import com.hfad.simplemanager.ui.theme.elevation
 import com.hfad.simplemanager.ui.theme.spacing
 import com.hfad.simplemanager.ui.theme.theme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
 @Composable
 fun TaskScreen(vm: TaskScreenVM = viewModel()) {
     val taskList by vm.taskFlow.collectAsState(listOf())
     val columnList by vm.taskListFlow.collectAsState(listOf())
     val selectedPrj by vm.selectedProjectFlow.collectAsState(initial = null)
-    
+
     val w = LocalConfiguration.current.screenWidthDp
+
+    val taskListListState = rememberLazyListState()
 
     if (selectedPrj == null) {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -48,26 +61,51 @@ fun TaskScreen(vm: TaskScreenVM = viewModel()) {
         return
     }
 
-    LazyRow(
-        modifier = Modifier
-            .padding(theme.spacing.medium)
-            .fillMaxSize()
-    ) {
-        items(columnList) { c ->
+    HorizontalPager(count = columnList.size + 1) { page ->
+        if (page == columnList.lastIndex + 1) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                NewTaskList(
+                    Modifier
+                        .width(w.dp)
+                        .align(Alignment.TopCenter)) {
+                    vm.handleEvent(
+                        TaskScreenEvent.NewTaskList(it)
+                    )
+                }
+            }
+        } else {
             TaskList(
                 modifier = Modifier.width(w.dp),
-                state = c,
+                state = columnList[page],
                 tasks = taskList,
                 destinations = columnList,
                 taskEventHandler = { vm.handleEvent(it) },
                 handle = { vm.handleEvent(it) }
             )
         }
-
-        item {
-            NewTaskList(Modifier.width(w.dp)) { vm.handleEvent(TaskScreenEvent.NewTaskList(it)) }
-        }
     }
+//
+//    LazyRow(
+//        modifier = Modifier
+//            .padding(theme.spacing.medium)
+//            .fillMaxSize(),
+//        userScrollEnabled = true
+//    ) {
+//        items(columnList) { c ->
+//            TaskList(
+//                modifier = Modifier.width(w.dp),
+//                state = c,
+//                tasks = taskList,
+//                destinations = columnList,
+//                taskEventHandler = { vm.handleEvent(it) },
+//                handle = { vm.handleEvent(it) }
+//            )
+//        }
+//
+//        item {
+//            NewTaskList(Modifier.width(w.dp)) { vm.handleEvent(TaskScreenEvent.NewTaskList(it)) }
+//        }
+//    }
 }
 
 @Composable
